2e9c5c3219a4b2b715de76408c915d3f114aa93d
bennicholes <nicholes.ben@gmail.com>
2015-11-05 13:03:54 -0800

    get rid of extra folder

--- StackQueue_Adapter_Patter/src/QueueInterface.java
+++
@@ -1,9 +0,0 @@
-//Implemented by Benjamin Nicholes
-
-public interface QueueInterface {
-
-		public int Size();
-		public boolean Empty();
-		public void Enqueue(int value);
-		public int Dequeue();
-}
--- StackQueue_Adapter_Patter/src/StackInterface.java
+++
@@ -1,8 +0,0 @@
-
-public interface StackInterface {
-
-	public int getSize();
-	public boolean isEmpty();
-	public int pop();
-	public void push(int value);
-}
